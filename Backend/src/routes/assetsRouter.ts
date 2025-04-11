740a306f214bbcae6fc4bc76be47cab78eb9a72c
NikoZ21 <89398005+NikoZ21@users.noreply.github.com>
2025-04-11 17:00:09 +0400

    changed folder structure

--- Backend/src/routes/assetsRouter.ts
+++ Backend/src/routes/assetsRouter.ts
@@ -5,8 +5,11 @@ const router = Router();
 router.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to the Assets Endpoint" });
 });
+router.get("/:id", (req: Request, res: Response) => {
+  res.json({ message: `Fetching asset with ID: ${req.params.id}` });
+});
+
 router.post("/", (req: Request, res: Response) => {});
-router.get("/:id", (req: Request, res: Response) => {});
 router.put("/:id", (req: Request, res: Response) => {});
 router.delete("/:id", (req: Request, res: Response) => {});
 
