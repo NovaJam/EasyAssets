ac9666f4b7d43b0a434fbaddf899211f1c0c3e17
Vishnu Kishore Tarini <tarinivishnukishore123@gmail.com>
2025-04-13 15:36:52 +0530

    feat: implement issue tracking dashboard with issue list and modals

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
+import IssueTrackerDashboard from "./pages/IssueTracker/IssueTrackerDashboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/issue-tracker" element={<IssueTrackerDashboard/>}/>
       </Routes>
     </BrowserRouter>
   );
