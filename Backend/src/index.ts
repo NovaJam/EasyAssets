b8af0dedba38212b15f2f6d9eaa5c39c778092c2
Avijit-ap <jitavi185@gmail.com>
2025-04-13 18:52:34 +0530

    feat: add nanoid for unique ID generation and update TypeScript target version

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,7 +6,9 @@ import authRoutes from './routes/authRouter';
 import cookieParser from 'cookie-parser';
 import { setupSwagger } from '../swagger';
 import morgan = require('morgan');
-
+import { nanoid } from 'nanoid';
+const id = nanoid();
+console.log(id)
 const app = express();
 const port = 5000;
 app.use(morgan('dev', {
