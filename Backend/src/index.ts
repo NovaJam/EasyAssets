34491ebaa5760a9af7dd196d3d4ac8cf716d5118
Kamlesh Suthar <kamleshsuthar240725@gmail.com>
2025-04-17 10:39:52 +0530

    updated the CORS Policy Backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -8,10 +8,14 @@ import cookieParser from "cookie-parser";
 import { setupSwagger } from "./swagger";
 import morgan = require("morgan");
 import assets from "./routes/assetsRouter";
+import cors from "cors";
 
 dotenv.config();
-const cors = require("cors");
-
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN, // http://localhost:3000 for development and http://easyassets.vercel.app for production
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(
@@ -25,6 +29,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors(corsOptions)); // Enable CORS with the specified options
 
 //For API Docs using Swagger UI
 setupSwagger(app);
